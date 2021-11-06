9dbad2987e98dd94dde4a2844789746f3f8aafe3
Tnoob-dev <93684341+Tnoob-dev@users.noreply.github.com>
2021-11-06 04:45:56 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,10 @@ import wget
 import os
 
 UploaderBot = Client(
-    "Uploaderbot",
-    bot_token= "TOKEN_FROM_BOTFATHER", #example: 1234567890:ABCDEFGHIJKLMNOPQRSTUVWXYZ
-    api_hash= "API Hash From my.telegram.org",
-    api_id= "API ID from my.telegram.org" #int valor (example:(int(12345)))
+    "UploaderBot",
+    bot_token ="2146516089:AAFnQXqcbpy1BSS7bts1qtS3-rJeMDj5mpY",
+    api_hash = "578d2817642f3aa3a283efaf49f4ef4e",
+    api_id = int(12845924)
 )
 
 
@@ -15,7 +15,7 @@ UploaderBot = Client(
 async def Start(filters, message):
     print(f"🤖The Bot was started by: {message.from_user.id}\n{message.from_user.username}\n") #In Spanish: "El bot ha sido comenzado por: "
     await message.reply_photo("https://telegra.ph/file/fad940fbefd120bd58200.png")
-    await message.reply_text(f"🙃User: {message.from_user.mention}\n🆔ID: {message.from_user.id}\n📛Username: @{message.from_user.username}\n\n🇬🇧Hi Human, I'm a **Uploader to Telegram bot**, i can upload files from some sites(Example: Uptodown.com, Malavida.com, etc..., just simple sites, in other actualizations will come the Clouds and Youtube), not at all, i can't upload from MEGA, Gdrive, OneDrive, etc...,Here you can see more info about my Creator😁👇\n\n\n🇪🇸Hola Humano, Soy un **Bot que sube archivos a Telegram**, puedo subir archivos directos desde varios sitios(Ejemplos: Uptodown.com, Malavida.com, etc..., solo sitios simples, en proximas actualizaciones podras descargar desde nubes y Youtube), no todos, no puedo subir desde MEGA, Gdrive, OneDrive, etc...,Aqui puedes ver mas info acerca de mi creador😁👇",
+    await message.reply_text(f"🙃User: {message.from_user.mention}\n🆔ID: {message.from_user.id}\n📛Username: @{message.from_user.username}\n\n🇬🇧Hi Human, I'm a **Uploader to Telegram bot**, i can upload files from some sites(Example: Uptodown.com, Malavida.com, etc..., just simple sites, in other actualizations will come the Clouds and Youtube), not at all, i can't upload from MEGA, Gdrive, OneDrive, etc...,Here you can see more info about my Creator😁👇\n\n\n🇪🇸Hola Humano, Soy un **Bot que suber archivos a Telegram**, puedo subir archivos directos desde varios sitios(Ejemplos: Uptodown.com, Malavida.com, etc..., solo sitios simples, en proximas actualizaciones podras descargar desde nubes y Youtube), no todos, no puedo subir desde MEGA, Gdrive, OneDrive, etc...,Aqui puedes ver mas info acerca de mi creador😁👇",
     
     reply_markup=InlineKeyboardMarkup([[
         InlineKeyboardButton("👾Github👾", url="https://www.github.com/Tnoob-dev"),
@@ -29,27 +29,29 @@ async def Start(filters, message):
 @UploaderBot.on_message(filters.command("help") & ~filters.edited)
 async def ayuda(filters, message):
     await message.reply_photo("https://telegra.ph/file/a09ec751bdfd44711504a.png")
-    await message.reply_text("🇬🇧Just send me a link and i will try to download it\n\n🇪🇸Solo enviame un link y lo intentare descargar")
+    await message.reply_text("🇬🇧Just send me a link and i will try to download it\n\nNote: If you want to upload some other things and the bot don't recongnize him, you can go to @DirectLinkGeneratorbot, @DirectLinkGen_bot or @MaxFile2LinkBot; i recommend this last, is one of the bests i was used.\n\n🇪🇸Solo enviame un link y lo intentare descargar\n\n**Nota**: Si envias un enlace y el bot no lo reconoce, puedes enviar el enlace a @DirectLinkGeneratorbot, @DirectLinkGen_bot o @MaxFile2LinkBot; recomiendo este ultimo, es uno de los mejores que he usado\n")
 
 @UploaderBot.on_message(filters.regex(pattern=".*http.*") & ~filters.edited) #the filters and that stuffs
 async def descargar(client, message: Message):
     msg = await message.reply_text(text="🔎Checking URL🔎", quote=True) #Check the URL before Download
-    dwnlad = message.text #Link of the User
+    dwnlad = message.text #Link sended by the User
     photo = "https://telegra.ph/file/fad940fbefd120bd58200.png" #Photo of the archive, you can delet it if you don't want a photo in the archive, or just change it for other
     pal = "😉Here you have😉" #The text who will be at the side of the archive when this is uploaded
     try:
         await msg.edit("⬇️Trying to Download the archive⬇️") #this will be edit "Checking URL" when he check the URL
-        arch = wget.download(dwnlad)#Download the archive
-        photo = wget.download(photo)#Download the photo wh will be with the archive
+        filename = wget.download(dwnlad)#Download the archive
+        photo_file = wget.download(photo)#Download the photo who will be with the archive
         pak = "fad940fbefd120bd58200.png" #Name + extension of the photo 
         await msg.edit("⬆️Uploading Archive⬆️")#this will be edit "Trying to Download Archive" when the archive is downloaded
-        await message.reply_document(arch, thumb=pak, caption=pal) #Here we upload the archive
-        await msg.delete()#Delete the message when Upload the archive
+        await message.reply_document(filename, thumb=pak, caption=pal) #Here we upload the archive
+        await msg.edit("✅✅Upload Success✅✅")#Delete the message when Upload the archive
         #Remove from server
-        os.remove(dwnlad)
-        os.remove(photo)
+        os.remove(filename)
+        os.remove(photo_file)
     except Exception:
         await msg.edit("❌❌Link not supported❌❌")#This he will show if the link is invalid
-        
+
+
+
 print("Bot running")
 UploaderBot.run()
