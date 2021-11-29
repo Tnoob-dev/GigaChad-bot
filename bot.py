ab1e7db827484aeaba8d7540e026af1fe117a734
Tnoob-dev <93684341+Tnoob-dev@users.noreply.github.com>
2021-11-28 19:44:07 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,31 @@
+import os
 from pyrogram import Client, filters
-import pyrogram 
+import pyrogram
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 import configparser
 import asyncio
 import tgcrypto
 import yt_dlp
 from yarl import URL
+import pyshorteners
+import qrcode
 
 config = configparser.ConfigParser()
 config.read("config.ini")
 
 UploaderBot = Client(
     "Uploaderbot",
-    api_id = config.get('pyrogram', 'api_id'), #API_ID it's on the config.ini file
-    api_hash = config.get('pyrogram', 'api_hash'), #API_HASh it's on the config.ini file
-    bot_token = config.get('pyrogram', 'bot_token') #BOT_TOKEN it's on the config.ini file
+    api_id = config.get('pyrogram', 'api_id'),
+    api_hash = config.get('pyrogram', 'api_hash'),
+    bot_token = config.get('pyrogram', 'bot_token')
 )
-
-
 
 
 @UploaderBot.on_message(filters.command("start") & ~filters.edited)
 async def Start(filters, message):
     print(f"🤖The Bot was started by: {message.from_user.id}\n{message.from_user.username}\n") #In Spanish: "El bot ha sido comenzado por: "
     await message.reply_photo("https://telegra.ph/file/fad940fbefd120bd58200.png")
-    await message.reply_text(f"🙃User: {message.from_user.mention}\n🆔ID: {message.from_user.id}\n📛Username: @{message.from_user.username}\n\n🇬🇧Hi Human, I'm a **Uploader to Telegram bot**, i can upload files from some sites(Example: Uptodown.com, Malavida.com, Youtube.com, etc..., just simple sites, in other actualizations will come the Clouds and Youtube), not at all, i can't upload from MEGA, Gdrive, OneDrive, etc...,Here you can see more info about my Creator😁👇\n\n\n🇪🇸Hola Humano, Soy un **Bot que suber archivos a Telegram**, puedo subir archivos directos desde varios sitios(Ejemplos: Uptodown.com, Malavida.com, Youtube.com, etc..., solo sitios simples, en proximas actualizaciones podras descargar desde nubes y Youtube), no todos, no puedo subir desde MEGA, Gdrive, OneDrive, etc...,Aqui puedes ver mas info acerca de mi creador😁👇",
+    await message.reply_text(f"🙃User: {message.from_user.mention}\n🆔ID: {message.from_user.id}\n📛Username: @{message.from_user.username}\n\n🇬🇧Hi Human, I'm a **Uploader to Telegram bot, i can generate QR Codes too, and short URLs**, i can upload files from some sites(Example: Uptodown.com, Malavida.com, Youtube.com, etc..., just simple sites, in other actualizations will come the Clouds and Youtube), not at all, i can't upload from MEGA, Gdrive, OneDrive, etc...,Here you can see more info about my Creator😁👇\n\n\n🇪🇸Hola Humano, Soy un **Bot que sube archivos a Telegram, genera codigos QR y acorta enlaces**, puedo subir archivos directos desde varios sitios(Ejemplos: Uptodown.com, Malavida.com, Youtube.com, etc..., solo sitios simples, en proximas actualizaciones podras descargar desde nubes y Youtube), no todos, no puedo subir desde MEGA, Gdrive, OneDrive, etc...,Aqui puedes ver mas info acerca de mi creador😁👇",
     
     reply_markup=InlineKeyboardMarkup([[
         InlineKeyboardButton("👾Github👾", url="https://www.github.com/Tnoob-dev"),
@@ -36,11 +37,10 @@ async def Start(filters, message):
     )
     )
 
-
 @UploaderBot.on_message(filters.command("help") & ~filters.edited)
 async def ayuda(filters, message):
     await message.reply_photo("https://telegra.ph/file/a09ec751bdfd44711504a.png")
-    await message.reply_text("🇬🇧Just send /download, later send me a link and i will download it\n\nNote: If you want to upload some other things and the bot don't recongnize him, you can go to @DirectLinkGeneratorbot, @DirectLinkGen_bot or @MaxFile2LinkBot.\n\n🇪🇸Solo enviame /download, luego enviame un link y lo descargare\n\n**Nota**: Si envias un enlace y el bot no lo reconoce, puedes enviar el enlace a @DirectLinkGeneratorbot, @DirectLinkGen_bot o @MaxFile2LinkBot.\n")
+    await message.reply_text("🇬🇧Send /download and later send me the link, i will Download it and send you the file\n\nSend /qr and later a text and i will generate you a QR Code with the text\n\nSend /short and later send a link and i will short it\n\nNote: If you want to upload some other things and the bot don't recongnize him, you can go to @DirectLinkGeneratorbot, @DirectLinkGen_bot or @MaxFile2LinkBot.\n\n🇪🇸Envia /download y luego el link de descarga, lo descargare y luego te enviare el archivo\n\nEnvia /qr y luego un texto, y te generare un codigo QR con el texto dentro\n\nEnvia /short y luego un link y te enviare un link acortado con el link que enviaste\n\n**Nota**: Si envias un enlace y el bot no lo reconoce, puedes enviar el enlace a @DirectLinkGeneratorbot, @DirectLinkGen_bot o @MaxFile2LinkBot.\n")
 
 
 Conversation_state = {} 
@@ -48,13 +48,13 @@ Conversation_state = {}
 
 @UploaderBot.on_message() #the filters and that stuffs
 async def descargar(client, message: Message):
-    dwnlad = message.text #Link sended by the User
+    dwnlad = message.text #message sended by the User
     pal = f"✅✅Upload Success✅✅\nUploaded By @Uploader_Tbot\nRemember GigaChad Loves u😘\nThe file was requested by: {message.from_user.id}" #The text who will be at the side of the archive when this is uploaded
-    
+    pal_qr = f"✅✅QR Generated✅✅\nUploaded By @Uploader_Tbot\nRemember GigaChad Loves u😘\nThe QR was requested by: {message.from_user.id}"
     who = message.from_user.id
     state = Conversation_state.get(who)
     
-    DOWNLOAD_LINK = True
+    DOWNLOAD_LINK = 0
 
     if state is None and message.text == "/download":
         await message.reply_text("🙃Give me a link to download🙃")
@@ -64,16 +64,55 @@ async def descargar(client, message: Message):
 
     if state == DOWNLOAD_LINK and URL(dwnlad).scheme and URL(dwnlad).host :
         del Conversation_state[who]
+        m = await message.reply_text("⬇️Downloading the archive⬇️")
         try:
             loop = asyncio.get_running_loop()
 
             ytdownload = yt_dlp.YoutubeDL({"logger": YT_DLP_LOGGER()})
             fdata = await loop.run_in_executor(None, ytdownload.extract_info, dwnlad)
             fname = ytdownload.prepare_filename(fdata)
-
+            await asyncio.sleep(2)
+            await m.edit("**⬆️Uploading the archive⬆️**")
+            await asyncio.sleep(5)
+            await m.delete()
             await message.reply_document(fname, caption=pal)
         except Exception:
-            await message.reply_text("Not a link supported")
+            await message.reply_text("❌Not a link supported❌")
+
+
+        return
+    QR = 1
+    if state is None and message.text == "/qr":
+        await message.reply_text("✍️Send me the text to generate QR✍️")
+        Conversation_state[who] = QR
+
+    if state == QR and dwnlad:
+        del Conversation_state[who]
+        filename = "qr" + ".png"
+
+        img = qrcode.make(dwnlad)
+        img.save(filename)
+
+        await message.reply_photo(filename, caption=pal_qr)
+
+        os.remove(filename)
+        return
+
+    SHORT = 2
+    if state is None and message.text == "/short":
+        await message.reply_text("✍️Send me a link to short it✍️")
+        Conversation_state[who] = SHORT
+
+    if state == SHORT and dwnlad:
+        del Conversation_state[who]
+        
+        s = pyshorteners.Shortener()
+
+        short = s.clckru.short(dwnlad)
+
+        await message.reply_text(short)
+
+        return
 
 class YT_DLP_LOGGER(object):
     def debug(self, msg):
@@ -82,6 +121,8 @@ class YT_DLP_LOGGER(object):
             pass
     def warning(self, msg):
             pass
+
+
 
 print("Bot running")
 
