03281bfb956b3e2933799137a95434af405facd6
Tnoob-dev <93684341+Tnoob-dev@users.noreply.github.com>
2021-12-23 19:17:18 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+#Modules
 import os
 from pyrogram import Client, filters
 import pyrogram
@@ -5,33 +6,35 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 import asyncio
 import tgcrypto
 import yt_dlp
+from yt_dlp import YoutubeDL
 from yarl import URL
 import pyshorteners
 import qrcode
-import aiohttp
-import re
 from config import Config
 
-
-
+#Client
 GigaChad = Client(
     "GigaChad",
     api_id = Config.API_ID,
     api_hash = Config.API_HASH,
-    bot_token = Config.BOT_TOKEN"
+    bot_token = Config.BOT_TOKEN
 )
 
 
 @GigaChad.on_message(filters.command("start") & ~filters.edited)
 async def Start(filters, message):
     print(f"🤖The Bot was started by: {message.from_user.id}\n{message.from_user.username}\n") #In Spanish: "El bot ha sido comenzado por: "
-    await message.reply_photo("https://telegra.ph/file/fad940fbefd120bd58200.png")
-    await message.reply_text(f"🙃User: {message.from_user.mention}\n🆔ID: {message.from_user.id}\n📛Username: @{message.from_user.username}\n\n🇬🇧Hi Human, I'm a **Uploader to Telegram bot, i can generate QR Codes too, and short URLs**, i can upload files from some sites(Example: Uptodown.com, Malavida.com, Youtube.com, Google Drive,etc..., in other actualizations will come MEGA, and other amazing things), Here you can see more info about my Creator😁👇\n\n\n🇪🇸Hola Humano, Soy un **Bot que sube archivos a Telegram, genera codigos QR y acorta enlaces**, puedo subir archivos directos desde varios sitios(Ejemplos: Uptodown.com, Malavida.com, Youtube.com, Google Drive,etc..., en proximas actualizaciones podras descargar desde MEGA, y vendran mas cosas increibles), Aqui puedes ver mas info acerca de mi creador😁👇",
+    await message.reply_photo("https://telegra.ph/file/90aa09730e63d35357221.png")
+    await message.reply_text(f"🙃User: {message.from_user.mention}\n🆔ID: {message.from_user.id}\n📛Username: @{message.from_user.username}\n\n🇬🇧Hi Human, I'm a **Uploader to Telegram bot, i can generate QR Codes too, and short URLs**, i can upload files from some sites(Example: Uptodown.com, Malavida.com, Youtube.com, facebook.com,etc..., in other actualizations will come MEGA, and other amazing things), Here you can see more info about my Creator😁👇\n\n\n🇪🇸Hola Humano, Soy un **Bot que sube archivos a Telegram, genera codigos QR y acorta enlaces**, puedo subir archivos directos desde varios sitios(Ejemplos: Uptodown.com, Malavida.com, Youtube.com, facebook.com,etc..., en proximas actualizaciones podras descargar desde MEGA, y vendran mas cosas increibles), Aqui puedes ver mas info acerca de mi creador😁👇",
     reply_markup=InlineKeyboardMarkup([[
         InlineKeyboardButton("👾Github👾", url="https://www.github.com/Tnoob-dev"),
+
         InlineKeyboardButton("🐦Twitter🐦", url="https://twitter.com/TitiLM30")
     ],
-    [InlineKeyboardButton("📱User Telegram📱", url="https://t.me/TitiLM30")]
+    [InlineKeyboardButton("⌨️Canal interesante⌨️", url="https://t.me/s3softwareyprogramacion"),
+
+     InlineKeyboardButton("💻Grupo adjunto💻", url="https://t.me/S3SPGrupo")
+    ]
     ]
     
     )
@@ -39,8 +42,8 @@ async def Start(filters, message):
     await message.reply_text("**🇬🇧Note: Send /help to know what i can do\n\n🇪🇸Nota: Envia /help para conocer que puedo hacer**")
 @GigaChad.on_message(filters.command("help") & ~filters.edited)
 async def ayuda(filters, message):
-    await message.reply_photo("https://telegra.ph/file/a09ec751bdfd44711504a.png")
-    await message.reply_text("🇬🇧Send /download and later send me the link, i will Download it and send you the file\n\nSend /qr and later a text and i will generate you a QR Code with the text\n\nSend /short and later send a link and i will short it\n\nSend /gdown and i will download the link from GDrive\n\nNote: If you want to upload some other things and the bot don't recongnize him, you can go to @DirectLinkGeneratorbot, @DirectLinkGen_bot or @MaxFile2LinkBot.\n\n🇪🇸Envia /download y luego el link de descarga, lo descargare y luego te enviare el archivo\n\nEnvia /qr y luego un texto, y te generare un codigo QR con el texto dentro\n\nEnvia /short y luego un link y te enviare un link acortado con el link que enviaste\n\nEnvia /gdown e intentare descargar el enlace de GDrive\n\n**Nota**: Si envias un enlace y el bot no lo reconoce, puedes enviar el enlace a @DirectLinkGeneratorbot, @DirectLinkGen_bot o @MaxFile2LinkBot.\n")
+    await message.reply_photo("https://telegra.ph/file/86a8426af71ef4f7eeec8.png")
+    await message.reply_text("🇬🇧Send /download and later send me the link, i will Download it and send you the file\n\nSend /qr and later a text and i will generate you a QR Code with the text\n\nSend /short and later send a link and i will short it\n\nNote: If you want to upload some other things and the bot don't recongnize him, you can go to @DirectLinkGeneratorbot, @DirectLinkGen_bot or @MaxFile2LinkBot.\n\n🇪🇸Envia /download y luego el link de descarga, lo descargare y luego te enviare el archivo\n\nEnvia /qr y luego un texto, y te generare un codigo QR con el texto dentro\n\nEnvia /short y luego un link y te enviare un link acortado con el link que enviaste\n\nEnvia /gdown e intentare descargar el enlace de GDrive\n\n**Nota**: Si envias un enlace y el bot no lo reconoce, puedes enviar el enlace a @DirectLinkGeneratorbot, @DirectLinkGen_bot o @MaxFile2LinkBot.\n")
 
 
 Conversation_state = {} 
@@ -53,6 +56,7 @@ async def msg_handler(client, message: Message):
     pal_qr = f"✅✅QR Generated✅✅\nUploaded By @Uploader_Tbot\nRemember GigaChad Loves u😘\nThe QR was requested by: {message.from_user.id}"
     who = message.from_user.id
     state = Conversation_state.get(who)
+    chatid = message.chat.id
     
     DOWNLOAD_LINK = 0
 
@@ -73,6 +77,7 @@ async def msg_handler(client, message: Message):
             fdata = await loop.run_in_executor(None, ytdownload.extract_info, dwnlad)
             fname = ytdownload.prepare_filename(fdata)
             await asyncio.sleep(2)
+            await GigaChad.send_chat_action(chat_id = chatid,action="upload_document")
             await m.edit("**⬆️Uploading the archive⬆️**")
             await asyncio.sleep(5)
             await m.delete()
@@ -94,6 +99,7 @@ async def msg_handler(client, message: Message):
         img = qrcode.make(dwnlad)
         img.save(filename)
 
+        await GigaChad.send_chat_action(chat_id=chatid, action="upload_photo")
         await message.reply_photo(filename, caption=pal_qr)
 
         os.remove(filename)
@@ -113,35 +119,153 @@ async def msg_handler(client, message: Message):
         short_dagd = s.dagd.short(dwnlad)
         short_osdb = s.osdb.short(dwnlad)
 
+        await GigaChad.send_chat_action(chat_id=chatid, action="typing")
         await message.reply_text("✅✅Here you have your link shorted:\n\n😆From Clck.ru: \n\n" + short_clckru + "\n\n🙃From Da.gd: \n\n" + short_dagd + "\n\n😁From Os.db: \n\n" + short_osdb + "\n\n\nThanks for use @Uploader_Tbot😊", disable_web_page_preview=True)
 
         return
 
-    GDRIVE_DLD = 3
-    if state is None and message.text == "/gdown":
-        await message.reply_text("send me the link of Google Drive to Download it")
-        Conversation_state[who] = GDRIVE_DLD
+    DOWNLOAD_LINK2 = 3
+    if state is None and message.text == "/down_480":
+        await message.reply_text(f"Hola {message.from_user.first_name}, Enviame el link a descargar UwU") 
+        Conversation_state[who] = DOWNLOAD_LINK2
 
-    if state == GDRIVE_DLD and dwnlad:
+    if state == DOWNLOAD_LINK2 and dwnlad:
+
+        capt = f"Aqui tienes tu video guapo ( ˘ ³˘)♥️\n\n𝕮𝖔𝖕𝖞𝖗𝖎𝖌𝖍𝖙 @𝕿𝖎𝖙𝖎𝕷𝕸30\n\nEl archivo ha sido requerido por @{message.from_user.username}"
+
         del Conversation_state[who]
-        msg = await message.reply_text("⬇️**Downloading the file**⬇️")
-        async with aiohttp.ClientSession() as session:
-            async with session.get(dwnlad) as response:
 
-                content_disposition = response.headers["Content-Disposition"]
-                filename = "./" + re.search(r'filename="(.+?)"', content_disposition)[1] #for the name of the file
+        ydl_opts = {"format": "best[height<=480]"}
 
-                file = open(filename, "wb")#open the file
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
 
-                async for chunk in response.content.iter_chunked(64 * 1024): #chunk = size the file is going to be chipped to download, on this case 64Kb
-                    file.write(chunk)
-
-                file.close()
-        await asyncio.sleep(2)
-        await msg.edit("⬆️**Uploading the File**⬆️")
+        info = ydl.extract_info(dwnlad)     
+        msg_bot = await message.reply_text("⬇️**Downloading the file**⬇️")
+        await asyncio.sleep(3)
+        filen = ydl.prepare_filename(info)
+        await GigaChad.send_chat_action(chat_id = chatid,action="upload_video")
+        await msg_bot.edit("⬆️**Uploading the file**⬆️")
+        await msg_bot.delete()
         await asyncio.sleep(5)
-        await msg.delete()
-        await message.reply_document(filename, caption=pal)
+
+        await message.reply_document(filen, caption=capt)
+        
+        return
+
+
+    DOWNLOAD_LINK3 = 4
+    if state is None and message.text == "/down_360":
+        await message.reply_text(f"Hola {message.from_user.first_name}, Enviame el link a descargar UwU") 
+        Conversation_state[who] = DOWNLOAD_LINK3
+
+    if state == DOWNLOAD_LINK3 and dwnlad:
+
+        capt = f"Aqui tienes tu video guapo ( ˘ ³˘)♥️\n\n𝕮𝖔𝖕𝖞𝖗𝖎𝖌𝖍𝖙 @𝕿𝖎𝖙𝖎𝕷𝕸30\n\nEl archivo ha sido requerido por @{message.from_user.username}"
+
+        del Conversation_state[who]
+
+        ydl_opts = {"format": "best[height<=360]"}
+
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
+
+        info = ydl.extract_info(dwnlad)     
+        msg_bot = await message.reply_text("⬇️**Downloading the file**⬇️")
+        await asyncio.sleep(3)
+        filen = ydl.prepare_filename(info)
+        await GigaChad.send_chat_action(chat_id = chatid,action="upload_video")
+        await msg_bot.edit("⬆️**Uploading the file**⬆️")
+        await msg_bot.delete()
+        await asyncio.sleep(5)
+
+        await message.reply_document(filen, caption=capt)
+
+        return
+
+    DOWNLOAD_LINK4 = 5
+    if state is None and message.text == "/down_240":
+        await message.reply_text(f"Hola {message.from_user.first_name}, Enviame el link a descargar UwU") 
+        Conversation_state[who] = DOWNLOAD_LINK4
+    if state == DOWNLOAD_LINK4 and dwnlad:
+
+        capt = f"Aqui tienes tu video guapo ( ˘ ³˘)♥️\n\n𝕮𝖔𝖕𝖞𝖗𝖎𝖌𝖍𝖙 @𝕿𝖎𝖙𝖎𝕷𝕸30\n\nEl archivo ha sido requerido por @{message.from_user.username}"
+
+        del Conversation_state[who]
+
+        ydl_opts = {"format": "best[height<=240]"}
+
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
+
+        info = ydl.extract_info(dwnlad)     
+        msg_bot = await message.reply_text("⬇️**Downloading the file**⬇️")
+        await asyncio.sleep(3)
+        filen = ydl.prepare_filename(info)
+
+
+        await GigaChad.send_chat_action(chat_id = chatid,action="upload_video")
+
+
+        await msg_bot.edit("⬆️**Uploading the file**⬆️")
+        await msg_bot.delete()
+        await asyncio.sleep(5)
+
+        await message.reply_document(filen, caption=capt)
+
+        return
+
+    DOWNLOAD_LINK5 = 6
+    if state is None and message.text == "/down_144":
+        await message.reply_text(f"Hola {message.from_user.first_name}, Enviame el link a descargar UwU") 
+        Conversation_state[who] = DOWNLOAD_LINK5
+
+    if state == DOWNLOAD_LINK5 and dwnlad:
+
+        capt = f"Aqui tienes tu video guapo ( ˘ ³˘)♥️\n\n𝕮𝖔𝖕𝖞𝖗𝖎𝖌𝖍𝖙 @𝕿𝖎𝖙𝖎𝕷𝕸30\n\nEl archivo ha sido requerido por @{message.from_user.username}"
+
+        del Conversation_state[who]
+
+        ydl_opts = {"format": "best[height<=144]"}
+
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
+
+        info = ydl.extract_info(dwnlad)     
+        msg_bot = await message.reply_text("⬇️**Downloading the file**⬇️")
+        await asyncio.sleep(3)
+        filen = ydl.prepare_filename(info)
+        await GigaChad.send_chat_action(chat_id = chatid,action="upload_video")
+
+        await msg_bot.edit("⬆️**Uploading the file**⬆️")
+        await msg_bot.delete()
+        await asyncio.sleep(5)
+
+        await message.reply_document(filen, caption=capt)
+
+        return
+
+    DOWNLOAD_LINK6 = 7
+    if state is None and message.text == "/down_1080":
+        await message.reply_text(f"Hola {message.from_user.first_name}, Enviame el link a descargar UwU") 
+        Conversation_state[who] = DOWNLOAD_LINK6
+
+    if state == DOWNLOAD_LINK6 and dwnlad:
+
+        capt = f"Aqui tienes tu video guapo ( ˘ ³˘)♥️\n\n𝕮𝖔𝖕𝖞𝖗𝖎𝖌𝖍𝖙 @𝕿𝖎𝖙𝖎𝕷𝕸30\n\nEl archivo ha sido requerido por @{message.from_user.username}"
+
+        del Conversation_state[who]
+
+        ydl_opts = {"format": "best[height<=1080]"}
+
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
+
+        info = ydl.extract_info(dwnlad)     
+        msg_bot = await message.reply_text("⬇️**Downloading the file**⬇️")
+        await asyncio.sleep(3)
+        filen = ydl.prepare_filename(info)
+        await GigaChad.send_chat_action(chat_id = chatid,action="upload_video")
+        await msg_bot.edit("⬆️**Uploading the file**⬆️")
+        await msg_bot.delete()
+        await asyncio.sleep(5)
+
+        await message.reply_document(filen, caption=capt)
 
         return
 class YT_DLP_LOGGER(object):
@@ -151,7 +275,6 @@ class YT_DLP_LOGGER(object):
             pass
     def warning(self, msg):
             pass
-
 
 print("Bot running")
 
